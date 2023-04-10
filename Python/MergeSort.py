74c6dba134ef1eb011b93fe25d0c82c20f310e3e
ybostudent77 <91338727+ybostudent77@users.noreply.github.com>
2023-04-11 01:07:07 +0300

    merge sort!

---
+++ Python/MergeSort.py
@@ -0,0 +1,34 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        sub_array1 = arr[:mid]
+        sub_array2 = arr[mid:]
+
+        mergeSort(sub_array1)
+        mergeSort(sub_array2)
+
+        i = j = k = 0
+        
+        while i < len(sub_array1) and j < len(sub_array2):
+            if sub_array1[i] < sub_array2[j]:
+                arr[k] = sub_array1[i]
+                i += 1
+            else:
+                arr[k] = sub_array2[j]
+                j += 1
+            k += 1
+            
+        while i < len(sub_array1):
+            arr[k] = sub_array1[i]
+            i += 1
+            k += 1
+
+        while j < len(sub_array2):
+            arr[k] = sub_array2[j]
+            j += 1
+            k += 1
+
+
+arr = [11, 8, 3, 5, 7, 14]
+mergeSort(arr)
+print(arr)
